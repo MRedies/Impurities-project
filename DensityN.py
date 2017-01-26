64b5a686abb5c89397a85369ff08c84dce366e43
Maximilian Merte <maximilian.merte@rwth-aachen.de>
2017-01-26 11:46:12 +0100

    fixed DOS check

--- DensityN.py
+++ DensityN.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 def theta(x):
-    if(x.imag == 0 and x.real >= 0):
+    thres = 1e-8
+    if(np.abs(x.imag) < thres and x.real >= 0):
         return 1.0
     else:
         return 0.0
@@ -16,58 +17,63 @@ class DOS:
         self.alpBet = self.alpha**2 + self.beta**2
         self.E_so   = self.m * (self.alpha**2 + self.beta**2)
 
+
+    #derivative of delta function for delta(E-Eplus(k)) and delta(E-Eminus(k))
+    #evaluated with k+(E) and k-(E)
     def NPlusKp(self,E):
-        return 1./ np.abs(	1./self.m -  self.alpBet/np.sqrt(self.alpBet*self.kplus(E)**2+self.B0**2)	)
+        return np.abs(	self.kplus(E)*(-1./self.m -  self.alpBet/np.sqrt(self.alpBet*self.kplus(E)**2+self.B0**2))	)
 
     def NMinusKp(self,E):
-        return 1./ np.abs(	1./self.m + self.alpBet/np.sqrt(self.alpBet*self.kplus(E)**2+self.B0**2)	)
+        return np.abs(	self.kplus(E)*(-1./self.m + self.alpBet/np.sqrt(self.alpBet*self.kplus(E)**2+self.B0**2))	)
 
 
 
     def NPlusKm(self,E):
-        return 1./ np.abs(	1./self.m - self.alpBet/np.sqrt(self.alpBet*self.kminus(E)**2+self.B0**2)	)
+        return np.abs(	self.kminus(E)*(-1./self.m - self.alpBet/np.sqrt(self.alpBet*self.kminus(E)**2+self.B0**2))	)
 
     def NMinusKm(self,E):
-        return 1./ np.abs(	1./self.m + self.alpBet/np.sqrt(self.alpBet*self.kminus(E)**2+self.B0**2)	)
+        return np.abs(	self.kminus(E)*(-1./self.m + self.alpBet/np.sqrt(self.alpBet*self.kminus(E)**2+self.B0**2))	)
 
 
-
+    #k+(E) and k-(E)
+    #sol of E-Eminus(k)=0
     def kminus(self,E):
         return np.sqrt(	2*self.m*(   E + self.E_so - np.sqrt( (E+self.E_so)**2+self.B0**2-E**2)  )			)
 
+    #sol of E-Eplus(k)=0
     def kplus(self,E):
         return np.sqrt(	2*self.m*(   E + self.E_so + np.sqrt( (E+self.E_so)**2+self.B0**2-E**2)  )			)
     
 
     
     def N(self,E):
-        thres = 0
-        if(np.abs(self.kplus(E).imag) <= thres):
-            kp = 0.5 * (np.sign(self.kplus(E).real) + 1)
+        res = 0
+        if E<0:
+            res += theta(self.kplus(E)) *self.kplus(E) /self.NMinusKp(E)
+            res += theta(self.kminus(E))*self.kminus(E)/self.NMinusKm(E)
         else:
-            kp= 0
+            res += theta(self.kplus(E)) *self.kplus(E) /self.NMinusKp(E)
+            res += theta(self.kminus(E))*self.kminus(E)/self.NPlusKm(E)
+
+        return res/(2*np.pi)
 
 
-        if(np.abs(self.kminus(E).imag)<=thres):
-            km = 0.5 * (np.sign(self.kminus(E).real) + 1)
+    #contribution of kplus part to N
+    def Npl(self,E):
+        res = 0
+        if E<0:
+            res += theta(self.kplus(E)) *self.kplus(E) /self.NMinusKp(E)
         else:
-            km= 0
+            res += theta(self.kplus(E)) *self.kplus(E) /self.NMinusKp(E)
 
-        return (    kp*self.NPlusKp(E) + km*self.NMinusKm(E)) #/(2*np.pi)
-    
-    def N2(self, E):
-        return (theta(self.kplus(E))  * self.NPlusKp(E)
-            +   theta(self.kminus(E)) * self.NMinusKm(E))
+        return res/(2*np.pi)
 
-    def N3(self, E):
-        kp = self.kplus(E)
-        km = self.kminus(E)
+    #contribution of kminus part to N
+    def Nmi(self,E):
+        res = 0
+        if E<0:
+            res += theta(self.kminus(E))*self.kminus(E)/self.NMinusKm(E)
+        else:
+            res += theta(self.kminus(E))*self.kminus(E)/self.NPlusKm(E)
 
-        result  = theta(kp) * 1.0/(1.0/self.m  \
-                              +  self.alpBet/np.sqrt(self.alpBet * kp**2 + self.B0**2) )
-        result += theta(km) *1.0/(1.0/self.m \
-                              -  self.alpBet/np.sqrt(self.alpBet * km**2 + self.B0**2) )
-        return result
-
-    
-
+        return res/(2*np.pi)
--- main.py
+++ main.py
@@ -6,20 +6,20 @@ import matplotlib.pyplot as plt
 
 
 print("please select variable set:\n")
-print("0) m=1,  alpha=0, beta=0, B0=1")
-print("1) m=1,  alpha=1, beta=0, B0=0")
-print("2) m=1,  alpha=0, beta=1, B0=0")
-print("3) m=10, alpha=1, beta=0, B0=0")
-print("4) m=10, alpha=1, beta=0, B0=1")
+print("1) m=1,  alpha=0, beta=0, B0=1")
+print("2) m=1,  alpha=1, beta=0, B0=0")
+print("3) m=1,  alpha=0, beta=1, B0=0")
+print("4) m=10, alpha=1, beta=0, B0=0")
+print("5) m=10, alpha=1, beta=0, B0=1")
 
 #parameter
-i=int(input("set="))
+i=int(input("set="))-1
 
 
 
 
 
-E = np.linspace(-2, 8, 50)
+E = np.linspace(-8, 8, 50)
 sets = np.array([[1.0,1e-4,0.0,1.0],  
                  [1.0,1.0,0.0,0.0],
                  [1.0,0.0,1.0,0.0], 
@@ -35,21 +35,46 @@ b = DensityN.DOS(sets[i,0], sets[i,1], sets[i,2], sets[i,3])
 DOS_G    = np.zeros(E.shape)
 DOS_G_SA = np.zeros(E.shape)
 DOS_N    = np.zeros(E.shape)
+DOS_Npl  = np.zeros(E.shape)
+DOS_Nmi  = np.zeros(E.shape)
 DOS_N2   = np.zeros(E.shape)
 DOS_N3   = np.zeros(E.shape)
 
 for j in range(E.shape[0]):
     DOS_G[j]    = a.N(E[j])
    # DOS_G_SA[j] = a.N_small_angle(E[j])
-    DOS_N[j]    = b.N(E[j])
-    DOS_N2[j]   = b.N2(E[j])
-    DOS_N3[j]   = b.N3(E[j])
+    DOS_Npl[j]    = b.Npl(E[j])
+    DOS_Nmi[j]    = b.Nmi(E[j])
+    DOS_N[j]     = b.N(E[j])
+    #DOS_N2[j]   = b.N2(E[j])
+    #DOS_N3[j]   = b.N3(E[j])
 
-plt.plot(E,DOS_G,label='green')
-plt.plot(E,DOS_N,label="check")
-plt.plot(E,DOS_N3,  label="check 3")
+#plt.plot(E,DOS_G,label='green')
+plt.plot(E,DOS_N,label="DOS")
+plt.plot(E,DOS_Npl,label='k+ part')
+plt.plot(E,DOS_Nmi,label='k- part')
 
-plt.title("m="+str(sets[i,0])+"; alpha="+str(sets[i,1])+"; beta="+str(sets[i,2])+"; B0="+str(sets[i,3]))
+
+#plt.plot(E,DOS_N3,  label="check 3")
+
+plt.title("set "+str(i+1)+")  "+"m="+str(sets[i,0])+"; alpha="+str(sets[i,1])+"; beta="+str(sets[i,2])+"; B0="+str(sets[i,3]))
 #plt.ylim([-3,7])
 plt.legend()
 plt.show()
+
+
+'''
+#plot k+ and k-
+kpl = np.zeros(E.shape)
+kmi = np.zeros(E.shape)
+for j in range(E.shape[0]):
+    kpl[j] = b.kplus(E[j])
+    kmi[j] = b.kminus(E[j])
+
+plt.title('k(E)')
+plt.plot(E,kpl,label='+')
+plt.plot(E,kmi,label='-')
+plt.legend()
+plt.show()
+'''
+
